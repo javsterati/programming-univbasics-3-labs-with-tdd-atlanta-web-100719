3477528f51438f162426fcf93062ea06100deb53
Javier Huerta Jr <javsteratii@gmail.com>
2019-09-26 16:38:30 +0000

    Automatically backed up by Learn

---
+++ calculator.rb
@@ -0,0 +1,18 @@
+first_number = 5
+second_number = 10.4
+
+sum = first_number + second_number
+
+puts sum
+
+difference = first_number - second_number
+
+puts difference
+
+product = first_number * second_number
+
+puts product
+
+quotient = first_number / second_number
+
+puts quotient
